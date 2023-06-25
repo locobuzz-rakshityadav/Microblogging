f375949d70750aa1bf6f670050ab78b4b69adb15
rakshloco <137052602+rakshloco@users.noreply.github.com>
2023-06-25 14:15:37 +0530

    Add files via upload

---
+++ User Management Microservice/Data.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FollowAPI.Models;
+
+
+
+public class UserDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Your_SQL_Server_Connection_String");
+    }
+}
